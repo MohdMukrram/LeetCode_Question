5ddb308d8ba4a5d33b604cd79d6e48d80e61c05a
MohdMukrram <mukrrammalik9528@gmail.com>
2026-07-20 14:23:28 +0530

    Time: 0 ms (100%), Space: 153 MB (66.55%) - LeetHub

---
+++ 2856-minimum-array-length-after-pair-removals/2856-minimum-array-length-after-pair-removals.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minLengthAfterRemovals(vector<int>& nums) {
+        int n = nums.size();
+        int i=0;
+        int j=(n+1)/2;
+        int pairs = 0;
+        while(j<n){
+            if(nums[i]<nums[j]){
+                pairs++;
+                i++;
+                j++;
+            }
+            else j++;
+        }
+        return n - 2*pairs;
+    }
+};

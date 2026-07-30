fb02ae29e6471d1f2452d542f50922836e7c17c5
MohdMukrram <mukrrammalik9528@gmail.com>
2026-07-30 08:36:14 +0530

    Time: 0 ms (100%), Space: 8.5 MB (87.84%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0) return 0;
+
+        long long lo = 1;
+        long long hi = x;
+        int ans = 0;
+
+        while (lo <= hi) {
+            long long mid = lo + (hi - lo) / 2;
+
+            if (mid * mid <= x) {
+                ans = mid;
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};

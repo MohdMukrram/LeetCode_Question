4e8ace29c2bc41bd8f01baf5c80a43e18501e80a
MohdMukrram <mukrrammalik9528@gmail.com>
2026-07-30 13:51:00 +0530

    Time: 181 ms (21.09%), Space: 13.8 MB (40.45%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -10,14 +10,15 @@ public:
         // Recursive case: Check if s[i] == s[j] and the inner substring is a palindrome
         if (s[i] == s[j]) {
             return dp[i][j] = solve(s, i + 1, j - 1);
-        } else {
+        }
+        else {
             return dp[i][j] = false;
         }
     }
 
     string longestPalindrome(string s) {
         int n = s.size();
-        memset(dp, -1, sizeof(dp)); // Initialize dp with -1
+        memset(dp, -1, sizeof(dp));
         int maxlen = 0;
         int sp = 0;
         for (int i = 0; i < n; i++) {

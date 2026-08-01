19a0eb4f7cfc7a87146ba09d3248c02f5bf18b66
MohdMukrram <mukrrammalik9528@gmail.com>
2026-08-01 08:44:24 +0530

    Time: 50 ms (8.26%), Space: 28.9 MB (5.77%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,28 +1,45 @@
 class Solution {
 public:
-    void solve(vector<vector<char>>& arr , int i , int j , vector<vector<int>>& vis){
-        if(i < 0 || i>=arr.size() || j < 0 || j>=arr[0].size() || vis[i][j] == 1 || arr[i][j] != '1') return;
+    void solve(vector<vector<char>>& grid,int i,int j,vector<vector<int>>& vis){
+        int n = grid.size();
+        int m = grid[0].size();
+
+        queue<pair<int,int>> q;
+        vector<vector<int>> dir = {{0,1},{0,-1},{1,0},{-1,0}};
+
+        q.push({i,j});
         vis[i][j] = 1;
 
-        solve(arr,i,j+1,vis);
-        solve(arr,i,j-1,vis);
-        solve(arr,i+1,j,vis);
-        solve(arr,i-1,j,vis);
+        while(!q.empty()){
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+
+            for(int d=0;d<4;d++){
+                int nr = r + dir[d][0];
+                int nc = c + dir[d][1];
+
+                if(nr >=0 && nr<n && nc>=0 && nc<m && grid[nr][nc] == '1' && !vis[nr][nc]){
+                    q.push({nr,nc});
+                    vis[nr][nc] = 1;
+                }
+            }
+        }
     }
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-
-        vector<vector<int>> vis(n,vector<int>(m,0));
         int count = 0;
+        vector<vector<int>> vis(n,vector<int>(m,0));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j] == '1' && vis[i][j] != 1){
-                    count ++;
+                if(grid[i][j] == '1' && !vis[i][j]){
+                    count++;
                     solve(grid,i,j,vis);
                 }
             }
         }
+
         return count;
     }
 };

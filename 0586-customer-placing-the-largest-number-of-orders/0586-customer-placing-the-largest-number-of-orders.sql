0089dface1c9a0c756a64aa22380451c04664278
MohdMukrram <mukrrammalik9528@gmail.com>
2026-08-17 16:26:05 +0530

    Time: 486 ms (63.29%), Space: 0B (100%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -1,5 +1,19 @@
+WITH customer_count AS (
+    SELECT
+        customer_number,
+        COUNT(*) AS large_cust
+    FROM Orders
+    GROUP BY customer_number
+),
+ranked AS (
+    SELECT
+        customer_number,
+        large_cust,
+        ROW_NUMBER() OVER (
+            ORDER BY large_cust DESC
+        ) AS rnk
+    FROM customer_count
+)
 SELECT customer_number
-FROM Orders
-GROUP BY customer_number
-ORDER BY COUNT(*) DESC
-LIMIT 1;
+FROM ranked
+WHERE rnk = 1;

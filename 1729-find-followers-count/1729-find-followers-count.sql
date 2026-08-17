44ee6a6c93c2727c643824408250b7e6213821fd
MohdMukrram <mukrrammalik9528@gmail.com>
2026-08-17 17:03:30 +0530

    Time: 568 ms (67.39%), Space: 0B (100%) - LeetHub

--- 1729-find-followers-count/1729-find-followers-count.sql
+++ 1729-find-followers-count/1729-find-followers-count.sql
@@ -1,6 +1,12 @@
+WITH follower_count AS (
+    SELECT
+        user_id,
+        COUNT(*) AS followers_count
+    FROM Followers
+    GROUP BY user_id
+)
 SELECT
     user_id,
-    COUNT(*) AS followers_count
-FROM Followers
-GROUP BY user_id
+    followers_count
+FROM follower_count
 ORDER BY user_id;

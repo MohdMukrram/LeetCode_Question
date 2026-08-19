d0851e50e09721b52dce33176354b3e653deb8a2
MohdMukrram <mukrrammalik9528@gmail.com>
2026-08-19 08:41:37 +0530

    Time: 24 ms (75.34%), Space: 50.6 MB (7.03%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
@@ -9,8 +9,6 @@
  */
 class Codec {
 public:
-
-    // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
         if(!root) return "null,";
         return to_string(root->val) + "," + serialize(root->left) + serialize(root->right);
@@ -21,14 +19,11 @@ public:
             index++;
             return NULL;
         }
-        // create node
         TreeNode* node = new TreeNode(stoi(nodes[index++]));
-        node->left = build(nodes, index);   // recursively build left
-        node->right = build(nodes, index);  // recursively build right
+        node->left = build(nodes, index);
+        node->right = build(nodes, index);
         return node;
     }
-
-    // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
         vector<string> nodes;
         string val;
@@ -41,7 +36,3 @@ public:
         return build(nodes, index);
     }
 };
-
-// Your Codec object will be instantiated and called as such:
-// Codec ser, deser;
-// TreeNode* ans = deser.deserialize(ser.serialize(root));

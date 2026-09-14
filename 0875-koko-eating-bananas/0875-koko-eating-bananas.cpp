3ab2e52197aa54ebf785fff78956a4f257adf815
MohdMukrram <mukrrammalik9528@gmail.com>
2026-09-14 10:55:52 +0530

    Time: 13 ms (21.9%), Space: 23.1 MB (5.76%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    bool check(vector<int>& arr , int mid , int h){
-        int n = arr.size();
-        long long ans = 0;
+    bool solve(vector<int>& piles, int k , int h){
+        long long hours = 0;
+        for(int i=0;i<piles.size();i++){
+            hours += ceil(piles[i]/(double)k);
 
-        for(int i=0;i<n;i++){
-            ans += ceil(arr[i]/(double)mid);
-            if(ans > h) return false;
         }
-
-        return ans <= h;
+        return hours <= h;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        int lo = 1;
-        int hi = *max_element(piles.begin(),piles.end());
-
-        int ans = 0;
-
+        int mx = *max_element(piles.begin(),piles.end());
+        int lo = 1, hi = mx;
+        
+        int ans = -1;
         while(lo<=hi){
             int mid = lo + (hi-lo)/2;
-            if(check(piles,mid,h)){
+            if(solve(piles,mid,h)){
                 ans = mid;
                 hi = mid-1;
             }
-            else lo = mid+1;
+            else lo = mid + 1;
         }
         return ans;
     }

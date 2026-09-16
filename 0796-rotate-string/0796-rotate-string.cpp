8853a70c7cdb766eaff67b8c56d144f507af4b72
MohdMukrram <mukrrammalik9528@gmail.com>
2026-09-16 09:50:53 +0530

    Time: 0 ms (100%), Space: 8.1 MB (23.59%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,36 +1,15 @@
 class Solution {
 public:
-    bool KMP(string text, string pattern){
-        int n = text.size(), m = pattern.size();
-        vector<int> lps(m);
-
-        // build lps
-        for(int i=1,len=0;i<m;){
-            if(pattern[i]==pattern[len]){
-                lps[i++] = ++len;
-            } else if(len){
-                len = lps[len-1];
-            } else {
-                lps[i++] = 0;
-            }
-        }
-
-        // search
-        for(int i=0,j=0;i<n;){
-            if(text[i]==pattern[j]){
-                i++; j++;
-            }
-            if(j==m) return true;
-            else if(i<n && text[i]!=pattern[j]){
-                if(j) j = lps[j-1];
-                else i++;
+    bool rotateString(string s, string goal) {
+        int n = s.size();
+        int m = goal.size();
+        if (n != m) return false;
+        string doubled = s + s;
+        for (int i = 0; i <= doubled.size() - m; i++) {
+            if (doubled.substr(i, m) == goal) {
+                return true;
             }
         }
         return false;
-    }
-
-    bool rotateString(string s, string goal) {
-        if (s.size() != goal.size()) return false;
-        return KMP(goal + goal, s);
     }
 };
